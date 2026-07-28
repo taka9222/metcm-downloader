5dd20f03a61f2c95bef68beed505e17d688768e9
taka9222 <yuritoya.88112@gmail.com>
2026-07-28 20:36:17 +0900

    update

--- main.py
+++ main.py
@@ -95,19 +95,19 @@ def settings_page():
     with ui.column().classes("w-full gap-3"):
 
         with ui.row().classes("w-full items-center"):
-            ui.label("表示テーマ").classes("w-40")
+            ui.label("表示テーマ").classes("w-30")
             ui.select(
                 options=DISPLAY_THEMES,
                 value=app.storage.user.get(DISPLAY_THEME_KEY, "system"),
                 on_change=change_theme,
-            ).props("outlined dense").classes("flex-1")
+            ).props("outlined dense")
 
         with ui.row().classes("w-full items-center"):
-            ui.label("表示単位").classes("w-40")
+            ui.label("表示単位").classes("w-30")
             ui.select(
                 options=["m/s", "kt"],
                 value="m/s",
-            ).props("outlined dense").classes("flex-1")
+            ).props("outlined dense")
 
     floating_nav("settings")
 
